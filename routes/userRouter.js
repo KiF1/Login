affca9e584f9f2b878bcf35575cd48a6c6b3726f
Abner Santos <abner.kif1@gmail.com>
2022-03-23 11:04:23 -0300

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controller/userController')
 const auth = require('../controller/authController')
 
- router.get('/', auth.verifyLog,  (req,res)=> res.render('init', {error: false, body:{}}))
+ router.get('/',(req,res)=> res.render('init', {error: false, body:{}}))
 router.get('/register', (req,res)=> res.render('regist', {error: false, body:{}}))
 router.get('/logged', auth.verifyLog, (req,res)=>{
     res.render('logged')
